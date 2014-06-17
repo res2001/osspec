fe5f59fec7d4084d35d87829caa51333648feb3d
Borisov Alexey <borisov@lcard.ru>
2014-06-17 18:03:44 +0400

    fix: исправление кода ошибки для osspec_mutex_release

--- osspec.c
+++ osspec.c
@@ -15,8 +15,7 @@
   Вспомогательная функции для работы с мьютексами....
   **************************************************************************/
 //создание мьютекса
-t_mutex osspec_mutex_create(void)
-{
+t_mutex osspec_mutex_create(void) {
     t_mutex hnd;
 #ifdef _WIN32
     hnd = CreateMutex(NULL, FALSE, NULL);
@@ -24,17 +23,13 @@ t_mutex osspec_mutex_create(void)
         hnd = L_INVALID_MUTEX;
 #else
     hnd = (t_mutex)malloc(sizeof(pthread_mutex_t));
-    if (hnd!=NULL)
-    {
+    if (hnd!=NULL) {
         int err = pthread_mutex_init(hnd, NULL);
-        if (err)
-        {
+        if (err) {
             free(hnd);
             hnd = L_INVALID_MUTEX;
         }
-    }
-    else
-    {
+    } else {
         hnd = L_INVALID_MUTEX;
     }
 #endif
@@ -44,14 +39,12 @@ t_mutex osspec_mutex_create(void)
 
 
 //захват мьютекса
-int32_t osspec_mutex_lock(t_mutex handle, uint32_t tout)
-{
+int32_t osspec_mutex_lock(t_mutex handle, uint32_t tout) {
     int32_t res = 0;
     if (handle == L_INVALID_MUTEX)
         res = OS_SPEC_ERR_MUTEX_INVALID_HANDLE;
 #ifdef _WIN32
-    if (!res)
-    {
+    if (!res) {
         res = WaitForSingleObject(handle, tout)==WAIT_OBJECT_0 ? 0 : OS_SPEC_ERR_MUTEX_LOCK_TOUT;
     }
 #else
@@ -62,31 +55,28 @@ int32_t osspec_mutex_lock(t_mutex handle, uint32_t tout)
 }
 
 //освобождение мьютекса
-int32_t osspec_mutex_release(t_mutex handle)
-{
+int32_t osspec_mutex_release(t_mutex handle) {
     int32_t res = 0;
     if (handle == L_INVALID_MUTEX)
         res = OS_SPEC_ERR_MUTEX_INVALID_HANDLE;
 #ifdef _WIN32
     if (!res)
-        res = ReleaseMutex(handle) ? 0 : L502_ERR_MUTEX_RELEASE;
+        res = ReleaseMutex(handle) ? 0 : OS_SPEC_ERR_MUTEX_RELEASE;
 #else
     if (!res && pthread_mutex_unlock(handle))
-        res = L502_ERR_MUTEX_RELEASE;
+        res = OS_SPEC_ERR_MUTEX_RELEASE;
 #endif
     return res;
 }
 
-int32_t  osspec_mutex_close(t_mutex handle)
-{
+int32_t  osspec_mutex_close(t_mutex handle) {
     int32_t res = (handle != L_INVALID_MUTEX) ?
         0 : OS_SPEC_ERR_MUTEX_INVALID_HANDLE;
 #ifdef _WIN32
     if (!res)
         CloseHandle(handle);
 #else
-    if (!res)
-    {
+    if (!res) {
         pthread_mutex_destroy(handle);
         free(handle);
     }
